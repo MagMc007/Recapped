02ab6ad48943e5fb3b465f2c4ad2878d07d57415
MagMc007 <mergashasho7@gmail.com>
2025-10-08 10:06:54 +0300

    movie serializer preped

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
-from .models import models
+from .models import Movies
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    """ serializes movie objects """
+    class Meta:
+        model = Movies
+        fields = "__all__"
