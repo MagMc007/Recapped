7a6db36b9854dafcec701041d62b786d19cc8929
MagMc007 <mergashasho7@gmail.com>
2025-10-10 23:36:12 +0300

    added serializers for normalized fields

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,37 @@
 from rest_framework import serializers
-from .models import Movies
+from .models import Movies, Genre, Youtube
+
+
+class YTSerializer(serializers.ModelSerializer):
+    """ serilizes YT data related to object """
+    class Meta:
+        model = Youtube
+        fields = ['source_channel', 'source_channel_id', 'video_id']
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """ serilizies genre """
+    class Meta:
+        model = Genre
+        fields = ["name",]
 
 
 class MovieSerializer(serializers.ModelSerializer):
     """ serializes movie objects """
+    # prepare modelSers to nest
+    youtube_details = YTSerializer(read_only=True, many=True)
+    genre = GenreSerializer(read_only=True)
+
     class Meta:
         model = Movies
-        fields = "__all__"
+        fields = [
+            "id",
+            "name",
+            "country",
+            "year",
+            "genre",
+            "is_movie",
+            "is_series",
+            "poster_url",
+            "youtube_details",
+        ]
