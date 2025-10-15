be81e4da488f6aedce3a7bb0eebac0d3c5680f3e
MagMc007 <mergashasho7@gmail.com>
2025-10-15 13:53:15 +0300

    finished review listcreate view

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ReviewSerializer
+from .models import Rating, Movies
 
-# Create your views here.
+
+class ListCreateRating(generics.ListCreateAPIView):
+    """Allows user to rate a movie"""
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+      
+    def get_queryset(self):
+        movie_name = self.kwargs["movie_name"]
+        return Rating.objects.filter(movie__name=movie_name)
+
+    def perform_create(self, serializer):
+        movie_name = self.kwargs['movie_name']
+        movie = Movies.objects.get(name=movie_name)
+        serializer.save(user=self.request.user, movie=movie)
