9f68496f0fa43d5ac340c2ef1dbb2e9374d4ee55
MagMc007 <mergashasho7@gmail.com>
2025-10-17 21:10:44 +0300

    added components to navbar

--- frontend/Recapped/src/components/navbar.jsx
+++ frontend/Recapped/src/components/navbar.jsx
@@ -1,4 +1,6 @@
 import './css/navbar.css'
+// import { Link } from "react-router-dom"
+
 
 export default function NavBar() {
     return (
@@ -8,8 +10,29 @@ export default function NavBar() {
                 <div className="logo-cont">
                     <img src="blackLogo.png" alt="logo" />
                 </div>
-                <div className="links-cont"></div>
-                <div className="user-cont"></div>
+                <div className="links-cont">
+                    <div className="links">
+                        <a>Home</a>
+                        <a>Geners</a>
+                        <a>Country</a>
+                        <a>Year</a>
+                    </div>
+                    <div className="search-bar">
+                        <form action="">
+                            <input type="text" placeholder="Search Your Movie Here ..." />
+                            <button>Search</button>
+                        </form>
+                    </div>
+                </div>
+                <div className="user-cont">
+                    <div className="mode"></div>
+                    <div className="login-cont">
+                        Login
+                    </div>
+                    <div className="signup-cont">
+                        Sign Up
+                    </div>
+                </div>
             </div>
         </nav>  
         </>
