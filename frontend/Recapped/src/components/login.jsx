bd0a7a36b078bb3bf61f8b988961f022e4e073d8
MagMc007 <mergashasho7@gmail.com>
2025-10-19 20:48:00 +0300

    authentication pages complete

--- frontend/Recapped/src/components/login.jsx
+++ frontend/Recapped/src/components/login.jsx
@@ -1,16 +1,20 @@
 import './css/auth.css'
 import NavBar from './navbar.jsx'
+import {Link} from 'react-router-dom'
 
 export default function Login({light, setLight}){
     return (
         <>
             <NavBar light={light} setLight={setLight} />
-            <div className="auth-cont">
-                <div className="login-cont">
-                    
-                </div>
-                <div className="image-cont">
-                    <img src={light ? "whiteLogo.png": "blackLogo.png"} alt="image" />
+            <div className={light? "authentication-cont light-img": "authentication-cont dark-img"}>
+                <div className="login-auth-cont">
+                    <h1>Login</h1>
+                    <form action="">
+                        <input type="text" placeholder="Username"className={light? "light":"dark"} />
+                        <input type="password" placeholder="Password"className={light? "light":"dark"} />
+                        <button className={light? "auth light": "auth dark"}>Login</button>
+                    </form>
+                    <p>Don't have an account, <Link to="/signup">Sign Up</Link></p>
                 </div>
             </div>
         </>
--- frontend/Recapped/src/components/signup.jsx
+++ frontend/Recapped/src/components/signup.jsx
@@ -1,12 +1,24 @@
 import './css/auth.css'
 import NavBar from './navbar.jsx'
+import {Link} from 'react-router-dom'
 
 export default function SignUp({light, setLight}){
     return (
         <>
             <NavBar light={light} setLight={setLight} />
-            <div className="auth-cont">
-                <h1>This is signup page</h1>
+            <div className={light? "authentication-cont light-img": "authentication-cont dark-img"}>
+                <div className="login-auth-cont">
+                    <h1>Sign Up</h1>
+                    <div className="message"></div>
+                    <form action="">
+                        <input type="text" placeholder="Username" className={light? "light":"dark"} />
+                        <input type="email" placeholder="Email" className={light? "light":"dark"} />
+                        <input type="password" placeholder="Password" className={light? "light":"dark"} />
+                        <input type="password" placeholder="Confirmation" className={light? "light":"dark"} />
+                        <button className={light? "auth light": "auth dark"}>Sign Up</button>
+                    </form>
+                    <p>Already have an account, <Link to="/login">Login</Link></p>
+                </div>
             </div>
         </>
     )
