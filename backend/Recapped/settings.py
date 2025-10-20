2fbd8533a23f8597d5e87d144be60e4667b2c4aa
MagMc007 <mergashasho7@gmail.com>
2025-10-21 02:55:47 +0300

    fixed cors

--- backend/Recapped/settings.py
+++ backend/Recapped/settings.py
@@ -162,7 +162,10 @@ REST_FRAMEWORK = {
 }
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173", 
 ]
+
+CORS_ALLOW_CREDENTIALS = True
