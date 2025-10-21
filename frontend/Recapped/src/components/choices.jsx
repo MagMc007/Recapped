ff64311aaecd98b77ced44ef2b8dd91b14f84f61
MagMc007 <mergashasho7@gmail.com>
2025-10-21 15:47:01 +0300

    homepage styling

---
+++ frontend/Recapped/src/components/choices.jsx
@@ -0,0 +1,17 @@
+//import {Link} from 'react-router-dom'
+import './css/choices.css'
+
+export default function Choices() {
+    return (
+        <>
+            <div className="choices-cont">
+                <div className="choice movies">
+                    <p>Movies</p>
+                </div>
+                <div className="choice series">
+                    <p>Series</p>
+                </div>
+            </div>
+        </>
+    )
+}
--- frontend/Recapped/src/components/home.jsx
+++ frontend/Recapped/src/components/home.jsx
@@ -1,16 +1,20 @@
 import NavBar from './navbar.jsx'
+import  Choices from './choices.jsx'
 import './css/home.css'
 
 
 export default function Home({ light, setLight}) {
     document.title = "Home";
-    const username = sessionStorage.getItem("Username")
     return (
         <>
+            
             <NavBar light={light} setLight={setLight} />
+            
             <div className="home-cont">
-                <h1>This is Home page</h1>
-                <h2>Welcome {username}</h2>
+                <div className="home-choices">
+                   <Choices />
+                </div>
+             
             </div> 
         </>
     )
